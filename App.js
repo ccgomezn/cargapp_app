bdf1b5c0e218fdf83573c0e37eef856995279224
Brian Javier <brian-esguerra@upc.edu.co>
2019-07-16 14:47:30 -0500

    feat(theme/theme.style): add variables theme style

--- App.js
+++ App.js
@@ -8,15 +8,46 @@ import React, { Fragment } from 'react';
 import {
   SafeAreaView,
   ScrollView,
+  StyleSheet,
   View,
   Text,
   StatusBar,
 } from 'react-native';
 
+import { color, font } from './src/themes/theme.style';
+
+const styles = StyleSheet.create({
+  scrollView: {
+    backgroundColor: '#FFF',
+  },
+  body: {
+    backgroundColor: '#fff',
+  },
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 32,
+    alignContent: 'center',
+    justifyContent: 'center',
+    fontWeight: font.FONT_WEIGHT_LIGHT,
+    color: color.TEXT_COLOR_BLACK,
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+    color: color.TEXT_COLOR_GRAY,
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+});
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
+  constructor() {
+    super();
     this.state = {};
   }
 
@@ -27,19 +58,17 @@ class App extends React.Component {
         <SafeAreaView>
           <ScrollView
             contentInsetAdjustmentBehavior="automatic"
+            style={styles.scrollView}
           >
-            <View>
-              <View>
-                <Text>CargApp</Text>
-                <Text>
-                      Edit
-                  {' '}
-                  <Text>App.js</Text>
-                  {' '}
-                      Init Proyect
+            <View style={styles.body}>
+              <View style={styles.sectionContainer}>
+                <Text style={styles.sectionTitle}>Bienvenido al futuro</Text>
+                <Text style={styles.sectionDescription}>
+                  Edit
+                  <Text style={styles.highlight}>App.js</Text>
+                  Init Proyect
                 </Text>
               </View>
-
             </View>
           </ScrollView>
         </SafeAreaView>
