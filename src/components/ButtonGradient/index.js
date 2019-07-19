3843b230a94465f4fc991a7c6dc4796281adff93
JHouses <casasx12@hotmail.com>
2019-07-19 17:39:10 -0500

    feat(registration vehicle): screen registration

--- src/components/ButtonGradient/index.js
+++ src/components/ButtonGradient/index.js
@@ -1,17 +1,24 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Linear, Text } from './style';
+import { Linear, Text, Touch } from './style';
 
-function ButtonGradient({ content }) {
+function ButtonGradient({ content, press }) {
   return (
-    <Linear start={{ x: 0, y: 0 }} end={{ x: 1, y: 3 }} colors={['#007aff', '#00ff77']}>
-      <Text>{content}</Text>
-    </Linear>
+    <Touch onPress={press}>
+      <Linear
+        start={{ x: 0, y: 0 }}
+        end={{ x: 1, y: 3 }}
+        colors={['#007aff', '#00ff77']}
+      >
+        <Text>{content}</Text>
+      </Linear>
+    </Touch>
   );
 }
 
 ButtonGradient.propTypes = {
   content: PropTypes.string.isRequired,
+  press: PropTypes.func.isRequired,
 };
 
 export default ButtonGradient;
