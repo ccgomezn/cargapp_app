f249cb6fdb3556a0dc3fa394e2ca1a80cf37e045
JHouses <casasx12@hotmail.com>
2019-11-18 09:12:37 -0500

    redux state in origin travel

--- src/containers/Travels/StartTravel/index.js
+++ src/containers/Travels/StartTravel/index.js
@@ -71,11 +71,39 @@ export default class StartTravel extends Component {
   }
 
   ads(e) {
-    const { lastLat, lastLong } = this.state;
-    setTimeout(() => {
-      this.setState({ lastLat: e.latitude, lastLong: e.longitude });
-      this.callLocation();
-    }, 5000);
+    const { offerSpecific } = this.state;
+    if (offerSpecific.statu_id !== 7 && offerSpecific.statu_id !== 8) {
+      setTimeout(() => {
+        this.setState({ lastLat: e.latitude, lastLong: e.longitude });
+        this.callLocation();
+        this.destinationService(
+          e.latitude,
+          e.longitude,
+          offerSpecific.origin_latitude,
+          offerSpecific.origin_longitude,
+        );
+      }, 5000);
+    }
+  }
+
+  destinationService(lat1, lon1, lat2, lon2) {
+    if (lat1 === lat2 && lon1 === lon2) {
+      return 0;
+    }
+    const radLat1 = Math.PI * lat1 / 180;
+    const radLat2 = Math.PI * lat2 / 180;
+    const theta = lon1 - lon2;
+    const radTheta = Math.PI * theta / 180;
+    let dist = Math.sin(radLat1) * Math.sin(radLat2) + Math.cos(radLat1) * Math.cos(radLat2);
+    Math.cos(radTheta);
+    if (dist > 1) {
+      dist = 1;
+    }
+    dist = Math.acos(dist);
+    dist = dist * 180 / Math.PI;
+    dist = dist * 60 * 1.1515;
+    dist *= 1.609344;
+    return dist;
   }
 
   render() {
